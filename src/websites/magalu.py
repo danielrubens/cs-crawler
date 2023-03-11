6f7a604ee94431aff59349678f6bf8000d385acf
danielrubens <danielrubensdaniel@gmail.com>
2023-03-11 17:55:03 -0300

    magalu: get products html

--- src/websites/magalu.py
+++ src/websites/magalu.py
@@ -1,4 +1,6 @@
 import requests
+from typing import List
+from bs4 import BeautifulSoup
 from src.websites.website import HTML, Website
 
 
@@ -17,3 +19,9 @@ class Magalu(Website):
         urlify_product_name = product_name.lower().replace(" ", "+")
         return requests.get(
             f"{self.BASE_SEARCH_URL}/{urlify_product_name}", headers=self.HEADERS).text
+    
+    def _get_products_html(self, product_name: str) -> List[HTML]:
+        content = self._get_search_page_with_search_results(product_name)
+        soup = BeautifulSoup(content, "html.parser")
+        products = soup.find_all("li", {"class": "sc-fCBrnK hYPKVt"})
+        return [str(product) for product in products if products]
