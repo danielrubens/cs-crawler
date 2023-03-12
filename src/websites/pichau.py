25599e3b422f1a91822b0ad1678fd2aaad4bb3f2
danielrubens <danielrubensdaniel@gmail.com>
2023-03-12 02:01:33 -0300

    pichau: get products html

--- src/websites/pichau.py
+++ src/websites/pichau.py
@@ -1,5 +1,7 @@
-from src.websites.website import Website
 import requests
+from typing import List
+from bs4 import BeautifulSoup
+from src.websites.website import Website
 from src.websites.website import HTML
 
 class Pichau(Website):
@@ -15,3 +17,9 @@ class Pichau(Website):
 
     def _get_search_page_with_search_results(self, product_name: str) -> HTML:
         return requests.get(self.BASE_URL, headers=self.HEADERS, params={"q": product_name.lower()}).text
+
+    def _get_products_html(self, product_name: str) -> List[HTML]:
+        content = self._get_search_page_with_search_results(product_name)
+        soup = BeautifulSoup(content, "html.parser")
+        products = soup.find_all("a", {"data-cy": "list-product"})
+        return (str(product) for product in products)
